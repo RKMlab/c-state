74b504fcd8a31d041aaf21def8febb092c125d6f
sowpati <crackerjack.tej@gmail.com>
2016-12-05 19:49:20 +0530

    Added Settings Modal Changed plotscope settings object format

---
+++ scripts/views/settingsmodal.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const showSettings = function () {
+  settingsModal.showSettingsDiv = true;
+  $("#common-mask").css({"visibility":"visible", "opacity":"1"});
+  $("#settings-body").removeClass("slideOutLeft");
+  $("#settings-body").addClass("slideInLeft");
+}
+
+const hideSettingsDiv = function () {
+  $("#common-mask").css({"visibility":"hidden", "opacity":"0"});
+  $("#settings-body").removeClass("slideInLeft");
+  $("#settings-body").addClass("slideOutLeft");
+}
+
+const settingsModal = new Vue({
+  el: '#settings-modal',
+  data: {
+    showSettingsDiv: false,
+    genes: '',
+    scope: plotScope
+  },
+
+  methods: {
+    applySettings: function () {
+      // console.log(this.genes)
+      spinner.loading = true;
+      _.forEach(this.scope.genes, gene => {
+        gene.show = false;
+      });
+      _.delay(() => {
+        _.forEach(this.scope.genes, gene => gene.show = true)
+      }, 500);
+      // for (let i = 0; i < this.genes.length; i++) {
+      //   _.delay(() => {
+      //     this.genes[i].show = true;
+      //   }, i*5);
+      // }
+      _.delay(() => spinner.loading = false, (this.scope.genes.length+1) * 5);
+    }
+  }
+})
