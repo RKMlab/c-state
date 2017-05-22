4003873f769c05f8eb80f3842b8d7354dcaf8565
Divya Tej Sowpati <tej.sowpati@gmail.com>
2017-05-22 12:02:34 +0530

    Start of v0.3

--- scripts/components/generow.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const generow = Vue.component('generow', {
-  template: '#generow-template',
-  props: ['gene'],
-  data: function () {
-    return plotScope;
-  }
-});
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">{{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today'
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  font-family: 'Oxygen', Helvetica, sans-serif;
+}
+</style>
