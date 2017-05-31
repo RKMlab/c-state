c9c481cf657a1ae75287d6d6853c6fa9c649f1ed
Divya Tej Sowpati <tej.sowpati@gmail.com>
2017-05-31 16:08:59 +0530

    Updated BED parser

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,32 @@
   <div id="app">
     <el-row :gutter="20">
       <el-col :span="22" :offset="1">
-        <el-collapse :value="['2']">
-          <el-collapse-item title="Files" name="1">
+        <el-collapse :value="['1']">
+          <el-collapse-item name="1" title="Files">
             <el-row :gutter="20">
               <el-col :span="4" :offset="6">
-                <el-form>
-                  <el-form-item label="Select Genome">
-                    <el-select v-model="selectedSpecies" @change="onGenomeSelect">
-                      <el-option v-for="org in species" :key="org.name" :label="org.name" :value="org.name"></el-option>
-                    </el-select>
-                  </el-form-item>
-                </el-form>
+                <el-select v-model="store.info.selectedGenome" @change="onGenomeSelect">
+                  <el-option v-for="option in species" :key="option.name" :label="option.name" :value="option.name">
+                  </el-option>
+                </el-select>
               </el-col>
-              <el-col :span="4" :offset="2">
-                <el-form>
-                  <el-form-item label="Select Build">
-                    <el-select :disabled="selectedSpecies === ''" v-model="selectedBuild">
-                      <el-option v-for="build in buildOptions" :key="build" :label="build" :value="build"></el-option>
-                    </el-select>
-                  </el-form-item>
-                </el-form>
+              <el-col :span="4">
+                <el-select v-model="store.info.selectedVersion" @change="onVersionSelect">
+                  <el-option v-for="option in versionOptions" :key="option" :label="option" :value="option">
+                  </el-option>
+                </el-select>
+              </el-col>
+              <el-col :span="4" v-if="showFileUpload">
+                <label class="el-button el-button--primary">Upload Files
+                  <input type="file" multiple name="files" style="display: none" @change="onFeatureFileUpload">
+                </label>
+                <el-button :disabled="!showProcessButton" type="success" @click="processFiles">Process Files</el-button>
               </el-col>
             </el-row>
-            <transition appear name="fade">
-              <el-row :gutter="20">
-                <el-col :span="10" :offset="6" style="text-align: center">
-                  <el-button type="primary" :disabled="showGeneView" style="width: 200px">Go to GenomeView</el-button>
-                  <el-button type="primary" @click="showGeneView = true" style="width: 200px">Go to GeneView</el-button>
-                </el-col>
-              </el-row>
-            </transition>
-            <transition appear name="fade">
-              <gene-file-uploader></gene-file-uploader>
-            </transition>
           </el-collapse-item>
-          <el-collapse-item title="View" name="2">
-            <gene-view :store="store"></gene-view>
+        </el-collapse>
+        <el-collapse value="['1']">
+          <el-collapse-item name="1" title="View">
           </el-collapse-item>
         </el-collapse>
       </el-col>
@@ -46,42 +36,92 @@
 </template>
 
 <script>
-import Species from '../static/genomes.json'
-import store from './main.js'
-import GeneFileUploader from './components/gene_file_uploader.vue'
-import GeneView from './views/gene_view.vue'
+  import Species from '../static/genomes.json'
+  import { store } from './scripts/store.js'
+  import readFile from './scripts/utils/fileReader.js'
+  import { getGenomeString } from './scripts/createGenomeTemplate.js'
+  import { validateBED, parseBED } from './scripts/parsers/bed.js'
 
-const _ = require('lodash')
-export default {
-  components: {
-    GeneFileUploader, GeneView
-  },
-  data () {
-    return {
-      store: store,
-      species: Species.species,
-      selectedSpecies: '',
-      buildOptions: [],
-      selectedBuild: '',
-      showGeneView: false,
-      showGenomeView: false
-    }
-  },
-  methods: {
-    onGenomeSelect () {
-      this.buildOptions = _.find(this.species, ['name', this.selectedSpecies]).versions
-      this.selectedBuild = ''
+  const d3 = require('d3')
+  const _ = require('lodash')
+
+  export default {
+    data () {
+      return {
+        store: store,
+        species: Species.species,
+        files: '',
+        validated: [],
+        strings: [],
+        versionOptions: [],
+        showFileUpload: false
+      }
+    },
+    computed: {
+      showProcessButton () {
+        if (this.files.length === 0) {
+          return false;
+        }
+        return this.validated.length === this.files.length
+      }
+    },
+    methods: {
+      onGenomeSelect () {
+        this.versionOptions = _.find(this.species, ['name', this.store.info.selectedGenome]).versions
+      },
+      onVersionSelect () {
+        const { selectedGenome, selectedVersion } = this.store.info
+        d3.tsv(`/static/genomes/${selectedGenome}.${selectedVersion}.chromSizes`, data => {
+          this.store.info.chromSizes = data;
+          this.showFileUpload = true;
+        })
+      },
+      onFeatureFileUpload (evt) {
+        this.files = evt.target.files
+        const genome = this.store.info.chromSizes
+        for (let file of this.files) {
+          validateBED(file, 5, response => {
+            const [min, max] = response;
+            const obj = {
+              name: file.name,
+              min: min,
+              max: max,
+              file: file
+            }
+            this.validated.push(obj)
+          })
+        }
+      },
+      processFiles () {
+        let i = 0
+        const validated = this.validated
+        const context = this
+        let file = this.validated[i]
+        parseBED(file.file, 5, file.min, file.max, function callback(response) {
+          console.log(response)
+          i++;
+          if (i === validated.length) {
+            return;
+          }
+          const obj = {
+            name: file.name,
+            data: response
+          }
+          context.strings.push(obj)
+          file = validated[i]
+          parseBED(file.file, 5, file.min, file.max, callback)
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-body {
-  font-family: 'Oxygen', Helvetica, sans-serif;
-}
-
-.el-collapse-item__header {
-  font-size: 1em
-}
+  body {
+    font-family: 'Oxygen', Helvetica, sans-serif;
+  }
+  
+  .el-collapse-item__header {
+    font-size: 1em
+  }
 </style>
---
+++ src/scripts/store.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const store = {
+  constants: {
+    chromBinSize: 100
+  },
+  settings: {},
+  info: {
+    selectedGenome: '',
+    selectedVersion: '',
+    chromSizes: []
+  },
+  genes: []
+}
+
+export { store }
