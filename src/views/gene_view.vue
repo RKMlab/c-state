f789b9e84c9e9d00527f3fbdf1252be76bb967d9
Divya Tej Sowpati <tej.sowpati@gmail.com>
2017-05-31 10:30:47 +0530

    Updated eslintrc and package.json

--- src/views/gene_view.vue
+++ src/views/gene_view.vue
@@ -1,49 +1,49 @@
 <template>
   <div>
-  <input type="file" multiple name="files[]" @change="onFileUpload">
-  <div style="text-align: center">Hi</div>
+    <input type="file" multiple name="files[]" @change="onFileUpload">
+    <div style="text-align: center">Hi</div>
   </div>
 </template>
 
 <script>
-import readFile from '../scripts/utils/fileReader.js'
-import parseBED from '../scripts/parsers/bed.js'
-import { getGenomeString } from '../scripts/createGenomeTemplate.js'
+  import readFile from '../scripts/utils/fileReader.js'
+  import parseBED from '../scripts/parsers/bed.js'
+  import { getGenomeString } from '../scripts/createGenomeTemplate.js'
 
-const d3 = require('d3')
-export default {
-  name: 'GeneView',
-  props: ['store'],
-  data () {
-    return {
-      files: [],
-      strings: []
-    }
-  },
-  methods: {
-    onFileUpload (evt) {
-      const context = this
-      this.files = evt.target.files
-      for (let file of this.files) {
-        readFile(file, e => {
-          const fileData = parseBED(e.target.result, 5, file.name)
-          // console.log(fileData)
-          d3.tsv('/static/genomes/human.hg19.genome', function (genome) {
-            const obj = {
-              name: file.name
-            }
-            obj.data = getGenomeString(genome, fileData, 100)
-            context.strings.push(obj)
+  const d3 = require('d3')
+  export default {
+    name: 'GeneView',
+    props: ['store'],
+    data () {
+      return {
+        files: [],
+        strings: []
+      }
+    },
+    methods: {
+      onFileUpload (evt) {
+        const context = this
+        this.files = evt.target.files
+        for (let file of this.files) {
+          readFile(file, e => {
+            const fileData = parseBED(e.target.result, 5, file.name)
+            // console.log(fileData)
+            d3.tsv('/static/genomes/human.hg19.genome', function (genome) {
+              const obj = {
+                name: file.name
+              }
+              obj.data = getGenomeString(genome, fileData, 100)
+              context.strings.push(obj)
+            })
           })
-        })
+        }
       }
     }
   }
-}
 </script>
 
 <style scoped>
-div {
-  font-size: 1.2em;
-}
+  div {
+    font-size: 1.2em;
+  }
 </style>
